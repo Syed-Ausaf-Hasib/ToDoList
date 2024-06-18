f3e3bc1748aa2d4dc3722954be4ff3198edf35a5
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2024-06-18 22:15:54 +0530

    Initial project, ig complete

---
+++ app.js
@@ -0,0 +1,39 @@
+// i request express bodyparser 
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("Public"));
+app.set('view engine', 'ejs');
+
+var items = [];
+// var items=['Buy Food','Cook Food','Eat Food'];
+app.get("/", function (req, res) {
+    var today = new Date();
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+// app.get("/del",function(req,res){
+//     res.render("delete");
+// });
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.status(200).send("Item added successfully!");
+});
+
+app.listen(process.env.POST || 3000, function () {
+    console.log("Port is Running at 3000");
+});
